4f96ef656910d394c157ab8bef7568cadd0ba58a
Yifat <efrat0533193504@gmail.com>
2021-07-14 13:56:47 +0300

    trie_node

---
+++ trie_node.py
@@ -0,0 +1,20 @@
+class TrieNode:
+    alphabet_size = 27
+
+    # Trie node class
+    def __init__(self):
+        self.__children = [None] * type(self).alphabet_size
+        # isEndOfWord is True if node represent the end of the word
+        self.__is_end = False
+
+    def set_children(self, chr, val):
+        self.__children[ord(chr) - 97] = val
+
+    def get_child(self, chr):
+        return self.__children[ord(chr) - 97]
+
+    def get_is_end(self):
+        return self.__is_end
+
+    def set_is_end(self, val):
+        self.__is_end = val
